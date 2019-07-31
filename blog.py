002ba159b464359f31a770fa69a11cf59eadecfc
jsrglc <rgsyjb@163.com>
2019-07-31 08:54:35 +0800

    debug delete

--- blog.py
+++ blog.py
@@ -10,19 +10,37 @@ urls = (
 	'/new','New',
 	'/delete/(\d+)','Delete',
 	'/edit/(\d+)','Edit',
+	'/login','Login',
+	'/logout','Logout',
 	)
 
 ## Templates
 t_globals = {
-	'datestr': web.datestr
+	'datestr': web.datestr,
+	'cookie':  web.cookies,
 }
 render = web.template.render('templates', base='base', globals=t_globals)
+
+login = web.form.Form(
+	web.form.Textbox('username'),
+	web.form.Password('password'),
+	web.form.Button('login')
+	)
 
 class Index:
 	def GET(self):
 		"""Show page"""
+		login_form = login()
 		posts = model.get_posts()
-		return render.index(posts)
+		return render.index(posts, login_form)
+
+	def POST(self):
+		login_form = login()
+		if login_form.validates():
+			if login_form.d.username == 'admin' \
+			and login_form.d.password == 'admin':
+				web.setcookie('username', login_form.d.username)
+		raise web.seeother('/')
 
 class View:
 	def GET(self, id):
@@ -42,8 +60,11 @@ class New:
 		)
 
 	def GET(self):
-		form = self.form()
-		return render.new(form)
+		if not web.cookies().get('username'):
+			raise web.seeother('/')
+		else:
+			form = self.form()
+			return render.new(form)
 
 	def POST(self):
 		form = self.form()
@@ -53,6 +74,10 @@ class New:
 		raise web.seeother('/')
 
 class Delete:
+	def GET(self, id):
+		model.del_post(int(id))
+		raise web.seeother('/')
+
 	def POST(self, id):
 		model.del_post(int(id))
 		raise web.seeother('/')
@@ -71,6 +96,14 @@ class Edit:
 			return render.edit(post, form)
 		model.update_post(int(id), form.d.title, form.d.content)
 		raise web.seeother('/')
+
+class Logout:
+	def GET(self):
+		web.setcookie('username', '', expires=-1)
+		raise web.seeother('/')
+
+def notfound():
+	return web.notfound('Sorry, the page you were looking for was not found.')
 
 app = web.application(urls, globals())
 
